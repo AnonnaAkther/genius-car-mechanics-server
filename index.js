5e6c209eb85a42d53956cabb357c5ceb16f72d56
unknown <ah53622@gmail.com>
2021-11-20 18:31:58 +0600

    added port

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run(){
         });
 
         //DELETE API
-        app.get('/services/:id', async(req, res) => {
+        app.delete('/services/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await servicesCollection.deleteOne(query);
